f0dabe37bd0083c85d51dca86782ccd03812be19
Tony Fettes <tonyfettes@tonyfettes.com>
2024-04-05 02:40:09 +0800

    fix(librime): style

--- Formula/librime.rb
+++ Formula/librime.rb
@@ -2,8 +2,8 @@ class Librime < Formula
   desc "Rime Input Method Engine"
   homepage "https://rime.im"
   url "https://github.com/rime/librime.git",
-    using: :git,
-    tag: "1.11.0",
+    using:    :git,
+    tag:      "1.11.0",
     revision: "76a0a16c5ca0c7efc80fa918c8e0c88754699fd7"
   license "BSD-3-Clause"
 
