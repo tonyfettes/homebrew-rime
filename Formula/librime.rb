82056cebdadfbf57e95e0b05d15a3b4c11d91664
Tony Fettes <tonyfettes@tonyfettes.com>
2024-04-05 01:42:45 +0800

    fix(librime): pkg-config path

--- Formula/librime.rb
+++ Formula/librime.rb
@@ -11,19 +11,10 @@ class Librime < Formula
 
   def install
     ENV["PATH"] = "/usr/bin:/bin:/usr/sbin:/sbin:#{ENV["PATH"]}"
+    ENV["prefix"] = prefix
+    system "echo", "prefix=#{prefix}"
     system "make", "deps"
     system "make"
     system "make", "install"
-    bin.install "dist/bin/rime_deployer"
-    bin.install "dist/bin/rime_dict_manager"
-    bin.install "dist/bin/rime_patch"
-    bin.install "dist/bin/rime_table_decompiler"
-    include.install "dist/include/rime_levers_api.h"
-    include.install "dist/include/rime_api.h"
-    lib.install "dist/lib/librime.1.11.0.dylib"
-    lib.install "dist/lib/librime.1.dylib"
-    lib.install "dist/lib/librime.dylib"
-    lib.install "dist/lib/pkgconfig"
-    share.install "dist/share/cmake"
   end
 end
