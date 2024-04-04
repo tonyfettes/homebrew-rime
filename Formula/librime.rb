0636daa9004fff0bfc7d46f86d9172033c0db42f
Tony Fettes <tonyfettes@tonyfettes.com>
2024-04-05 02:32:44 +0800

    fix(librime): test-bot syntax

--- Formula/librime.rb
+++ Formula/librime.rb
@@ -1,12 +1,14 @@
 class Librime < Formula
   desc "Rime Input Method Engine"
   homepage "https://rime.im"
-  url "https://github.com/rime/librime.git", using: :git, tag: "1.11.0"
-  version "1.11.0"
+  url "https://github.com/rime/librime.git",
+    using: :git,
+    tag: "1.11.0",
+    revision: "76a0a16c5ca0c7efc80fa918c8e0c88754699fd7"
   license "BSD-3-Clause"
 
-  depends_on "cmake" => :build
   depends_on "boost" => :build
+  depends_on "cmake" => :build
   depends_on "icu4c" => :build
 
   def install
